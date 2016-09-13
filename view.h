deda21d0233ea91596b1fc4ca9f7fb4906e0e7c1
Speaker15071 <Speaker15071@gmail.com>
2016-09-13 15:20:31 +0500

    Create view.h

---
+++ view.h
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+sf::View view;
+
+void setPlayerCoordinateForView(float x, float y) {
+	float tempX = x; float tempY = y;
+
+	if (x < 320) tempX = 320;
+	if (y < 240) tempY = 240;
+	if (y > 624) tempY = 624;
+
+	view.setCenter(tempX, tempY);
+}
